f780eeda1db381544322bbf0de66b4e35f86497b
QA-Dmytro <Vasilina.dmitrii@gmail.com>
2023-09-10 01:12:21 +0300

    Merge remote-tracking branch 'origin/main'

--- src/test/java/init/InitialDriver.java
+++ src/test/java/init/InitialDriver.java
@@ -3,6 +3,7 @@ package init;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -11,19 +12,22 @@ import java.time.Duration;
 
 public class InitialDriver {
 
-    protected WebDriver driver;
-    protected WebDriverWait webDriverWait;
+    public WebDriver driver;
+    WebDriverWait webDriverWait;
 
     @BeforeTest
     public void setupDriver() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+//        WebDriverManager.chromedriver().setup();
+//        driver = new ChromeDriver();
+
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
         webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(5000));
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));
     }
-
     @AfterTest
     public void driverQuit() {
         driver.quit();
     }
+
 }
