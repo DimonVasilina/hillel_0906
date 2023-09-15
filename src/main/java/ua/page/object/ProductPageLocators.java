b5820d8ae81912042c14e74d1726749ac071e045
autodoc <alexeym@pkwteile.de>
2023-09-15 20:14:35 +0300

    lesson 27

---
+++ src/main/java/ua/page/object/ProductPageLocators.java
@@ -0,0 +1,10 @@
+package ua.page.object;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class ProductPageLocators {
+
+    By productTittle = By.xpath("//div[@class='product__heading']//h1");
+
+}
---
+++ src/main/java/ua/page/object/SearchPageLocators.java
@@ -0,0 +1,11 @@
+package ua.page.object;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class SearchPageLocators {
+
+    By firstProductTittle = By.xpath("//span[@class='goods-tile__title']");
+
+
+}
