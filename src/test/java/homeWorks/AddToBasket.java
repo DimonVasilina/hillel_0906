5ec076f569f3e105e548e97deeff9eadabd2e112
DimonVasilina <46611517+DimonVasilina@users.noreply.github.com>
2023-11-01 20:56:45 +0200

    Update AddToBasket.java

--- src/test/java/homeWorks/AddToBasket.java
+++ src/test/java/homeWorks/AddToBasket.java
@@ -1,1 +1,27 @@
+package homeWorks.hw33_selenideTest;
 
+import ua.hillel.homeworks.hw33_selenide.BasketSelenide;
+import ua.hillel.homeworks.hw33_selenide.HeaderSelenide;
+import ua.hillel.homeworks.hw33_selenide.MainPageSelenide;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class AddToBasket {
+
+    @Test
+    public void checkAddToBasketSelenide() {
+        open("https://www.rozetka.com.ua/");
+        String titleFirstProduct = new MainPageSelenide().goToCategory("Ноутбуки и компьютеры")
+                .goToSubCategory("Ноутбуки").addToBasket().getTitleProduct();
+        String counterBasket = new HeaderSelenide().getCounterBasket();
+        boolean checkBasketcaunter = counterBasket.equals("1");
+        Assert.assertTrue(checkBasketcaunter, "Basket counter isn`t correct");
+        String titleProductInBasket = new HeaderSelenide().goToBasket().getTitleInBasket();
+        String countOfItemInBasket = new BasketSelenide().getCountItem();
+        Assert.assertEquals(titleFirstProduct, titleProductInBasket, "Title aren`t equals");
+        Assert.assertEquals(counterBasket, countOfItemInBasket, "Added to Basket mostly");
+    }
+}
