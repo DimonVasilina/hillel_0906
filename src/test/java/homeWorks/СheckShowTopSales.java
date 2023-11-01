9ec8dd2e48b69d1b810566ccb9f9149009ba8d08
DimonVasilina <46611517+DimonVasilina@users.noreply.github.com>
2023-11-01 20:55:56 +0200

    Update СheckShowTopSales.java

--- src/test/java/homeWorks/СheckShowTopSales.java
+++ src/test/java/homeWorks/СheckShowTopSales.java
@@ -1,1 +1,20 @@
+package homeWorks.hw32;
 
+
+import init.InitialDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ua.hillel.homeworks.hw31_32.MainPage;
+
+public class СheckShowTopSales extends InitialDriver {
+
+    @Test
+    public void СheckShowTopSalesOnProductPage() {
+        driver.get("https://rozetka.com.ua/ua/");
+        boolean flagTopSales = new MainPage(driver)
+                .selectCategory().selectSubCategory()
+                .chooseSeller().chooseMaxPrice("100000").submitSorting()
+                .getTopSalesFirstCard().checkShowTopSalesFlag();
+        Assert.assertTrue (flagTopSales, "ТОП ПРОДАЖ is not displayed");
+    }
+}
